b8ec7132affcafc73054f82c55f3fcccbe6ba7fd
Kevin Lenell <krlenell@gmail.com>
2020-07-17 14:32:53 -0700

    created delete button

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -15,6 +15,7 @@ export default class GradeTable extends React.Component {
             <th scope="col">Name</th>
             <th scope="col">Course</th>
             <th scope="col">Grade</th>
+            <th scope="col">Operations</th>
           </tr>
         </thead>
         <tbody>
--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -8,6 +8,9 @@ export default class Grade extends React.Component {
         <td>{this.props.grade.name}</td>
         <td>{this.props.grade.course}</td>
         <td>{this.props.grade.grade}</td>
+        <td><button className="btn btn-danger">
+          <i className="fa fa-trash" aria-hidden="true"></i>
+        </button></td>
       </tr>
     );
   }
