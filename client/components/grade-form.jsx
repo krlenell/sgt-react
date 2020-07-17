b5e0824c694b8b66a55a744c77b03cfa6faa3662
Kevin Lenell <krlenell@gmail.com>
2020-07-17 11:29:05 -0700

    created bootstrap mess for form in gradeform

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -4,7 +4,30 @@ export default class GradeForm extends React.Component {
 
   render() {
     return (
-      <h1>GRADEFORM</h1>
+      <form>
+        <div className="input-group mb-1">
+          <div className="input-group-prepend">
+            <span className="input-group-text"><i className="fas fa-user"></i></span>
+          </div>
+          <input type="text" className="form-control" placeholder="Name" id="name"/>
+        </div>
+        <div className="input-group mb-1">
+          <div className="input-group-prepend">
+            <span className="input-group-text"><i className="fas fa-list-alt"></i></span>
+          </div>
+          <input type="text" className="form-control" placeholder="Course" id="course"/>
+        </div>
+        <div className="input-group mb-1">
+          <div className="input-group-prepend">
+            <span className="input-group-text"><i className="fas fa-graduation-cap"></i></span>
+          </div>
+          <input type="text" className="form-control" placeholder="Grade" id="grade"/>
+        </div>
+        <div className="d-flex justify-content-end">
+          <button type="submit" id="submit" className="btn btn-primary">Add</button>
+          <button type="reset" id="reset" className="btn ml-1 mr-1 btn-secondary">Reset</button>
+        </div>
+      </form>
     );
   }
 }
