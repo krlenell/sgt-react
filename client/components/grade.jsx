957eb5b38ac7e98ad7f1b85d7985a7fc1118a04d
Kevin Lenell <krlenell@gmail.com>
2020-10-10 15:22:55 -0700

    hookified grade.jsx

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 
-export default class Grade extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+export default function Grade({ grade, onSubmit }) {
+  function handleClick(event) {
+    const deleteId = grade.id;
+    onSubmit(deleteId);
   }
 
-  handleClick(event) {
-    const deleteId = this.props.grade.id;
-    this.props.onSubmit(deleteId);
-  }
-
-  render() {
-    return (
-      <tr>
-        <td>{this.props.grade.name}</td>
-        <td>{this.props.grade.course}</td>
-        <td>{this.props.grade.grade}</td>
-        <td><button onClick={this.handleClick} className="btn btn-danger">
-          <i className="fa fa-trash" aria-hidden="true"></i>
-        </button></td>
-      </tr>
-    );
-  }
+  return (
+    <tr>
+      <td>{grade.name}</td>
+      <td>{grade.course}</td>
+      <td>{grade.grade}</td>
+      <td><button onClick={handleClick} className="btn btn-danger">
+        <i className="fa fa-trash" aria-hidden="true"></i>
+      </button></td>
+    </tr>
+  );
 }
+
+// export default class Grade extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick(event) {
+//     const deleteId = grade.id;
+//     this.props.onSubmit(deleteId);
+//   }
+
+//   render() {
+//     return (
+//       <tr>
+//         <td>{this.props.grade.name}</td>
+//         <td>{this.props.grade.course}</td>
+//         <td>{this.props.grade.grade}</td>
+//         <td><button onClick={this.handleClick} className="btn btn-danger">
+//           <i className="fa fa-trash" aria-hidden="true"></i>
+//         </button></td>
+//       </tr>
+//     );
+//   }
+// }
