b13b0c3cf156c02a94558ed20ead5835c0a5f272
Kevin Lenell <krlenell@gmail.com>
2020-10-10 15:33:41 -0700

    hookified grade-table

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import Grade from './grade.jsx';
 
-export default class GradeTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getDeleteId = this.getDeleteId.bind(this);
+export default function GradeTable({ grades, onSubmit }) {
+
+  function getDeleteId(deleteId) {
+    onSubmit(deleteId);
   }
 
-  getDeleteId(deleteId) {
-    this.props.onSubmit(deleteId);
-  }
+  const gradeList = grades.map(grade => (
+    <Grade onSubmit={getDeleteId} key={grade.id} grade={grade}/>
+  ));
 
-  render() {
-    const grades = this.props.grades.map(grade => (
-      <Grade onSubmit={this.getDeleteId} key={grade.id} grade={grade}/>
-    ));
-
-    return (
-      <table className="table table-striped grade-table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Course</th>
-            <th scope="col">Grade</th>
-            <th scope="col">Operations</th>
-          </tr>
-        </thead>
-        <tbody>
-          {grades}
-        </tbody>
-      </table>
-    );
-  }
+  return (
+    <table className="table table-striped grade-table">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Course</th>
+          <th scope="col">Grade</th>
+          <th scope="col">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {gradeList}
+      </tbody>
+    </table>
+  );
 }
